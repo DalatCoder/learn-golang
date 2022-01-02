5e4be933559a950d8e2c4cd39d70d99a6b531df8
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-01-02 17:19:50 +0700

    function and handler

--- 03.go-modern-web/01.hello-world/main.go
+++ 03.go-modern-web/01.hello-world/main.go
@@ -5,15 +5,32 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(rw http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintln(rw, "<h1>Home page</h1>")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Number of bytes written to Home page", n)
+}
+
+// About is the about page handler
+func About(rw http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintln(rw, "<h1>About page</h1>")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Number of bytes written to About page", n)
+}
+
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintln(rw, "<h1>Hello World</h1>")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println("Number of bytes written", n)
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting the application on port:", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
--- 03.go-modern-web/README.md
+++ 03.go-modern-web/README.md
@@ -585,3 +585,44 @@ func main() {
 ```
 
 Start the application with: `go run main.go`
+
+### 2.2. Func and Handler
+
+```go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(rw http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintln(rw, "<h1>Home page</h1>")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Number of bytes written to Home page", n)
+}
+
+// About is the about page handler
+func About(rw http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintln(rw, "<h1>About page</h1>")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Number of bytes written to About page", n)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println("Starting the application on port:", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
+}
+```
