2539bb12c8505573cbe237fce492e82978960f7c
DalatCoder <hieuntctk42@gmail.com>
2022-07-01 22:07:34 +0700

    change created_at to NOT NULL

--- 06.simple-bank/db/migration/000001_init_schema.up.sql
+++ 06.simple-bank/db/migration/000001_init_schema.up.sql
@@ -1,24 +1,27 @@
-CREATE TABLE "accounts" (
+CREATE TABLE "accounts"
+(
   "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
   "currency" varchar NOT NULL,
-  "created_at" timestamptz DEFAULT (now())
+  "created_at" timestamptz DEFAULT (now()) NOT NULL
 );
 
-CREATE TABLE "entries" (
+CREATE TABLE "entries"
+(
   "id" bigserial PRIMARY KEY,
   "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
-  "created_at" timestamptz DEFAULT (now())
+  "created_at" timestamptz DEFAULT (now()) NOT NULL
 );
 
-CREATE TABLE "transfers" (
+CREATE TABLE "transfers"
+(
   "id" bigserial PRIMARY KEY,
   "from_account_id" bigint NOT NULL,
   "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
-  "created_at" timestamptz DEFAULT (now())
+  "created_at" timestamptz DEFAULT (now()) NOT NULL
 );
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
@@ -27,15 +30,20 @@ ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
 
-CREATE INDEX ON "accounts" ("owner");
+CREATE INDEX ON "accounts"
+("owner");
 
-CREATE INDEX ON "entries" ("account_id");
+CREATE INDEX ON "entries"
+("account_id");
 
-CREATE INDEX ON "transfers" ("from_account_id");
+CREATE INDEX ON "transfers"
+("from_account_id");
 
-CREATE INDEX ON "transfers" ("to_account_id");
+CREATE INDEX ON "transfers"
+("to_account_id");
 
-CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+CREATE INDEX ON "transfers"
+("from_account_id", "to_account_id");
 
 COMMENT ON COLUMN "entries"."amount" IS 'can be positive or negative';
 
